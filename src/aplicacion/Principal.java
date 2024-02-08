6e80aaaba57f8f0fed0e12558d55eb9844c7c00c
IvanRengelFerreira <ivan.rengelferreira@usp.ceu.es>
2024-02-08 14:21:11 +0100

    Avances jueves

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,5 +1,6 @@
 package aplicacion;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import mates.Funciones;
@@ -8,20 +9,49 @@ public class Principal {
 
    
     public static void main(String[] args){
-    Scanner sc  = new Scanner(System.in);
-    System.out.println("Elige un numero");
-    int numero = sc.nextInt();
 
-    System.out.println("La suma de los primeros"+numero +"números es: " + Funciones.suma(numero));
-    System.out.println("Elige un numero");
-    numero = sc.nextInt();
-    System.out.println("El factorial de "+numero+" es "+Funciones.factorial(numero));
-    System.out.println("Elige un numero");
-    numero = sc.nextInt();
-    int potencia = sc.nextInt();
-    System.out.println(numero+"a la"+potencia+"es"+Funciones.potencias(potencia, numero));
+    Scanner sc  = new Scanner(System.in);
+
+    // System.out.println("Elige un numero");
+    // int numero = sc.nextInt();
+    // System.out.println("La suma de los primeros "+numero +" números es: " + Funciones.suma(numero));
+
+    // System.out.println("Elige un numero");
+    // numero = sc.nextInt();
+    // System.out.println("El factorial de "+numero+" es "+Funciones.factorial(numero));
+
+    // System.out.println("Elige un numero");
+    // numero = sc.nextInt();
+    // System.out.println("Elige el numero por el que lo potencias");
+    // int potencia = sc.nextInt();
+    // System.out.println(numero+" a la "+potencia+" es "+Funciones.potencias(potencia, numero));
+
+    // System.out.println("Elige la cantidad de numero que quieres tener en la lista");
+    // Integer tamaño = sc.nextInt();
+    // System.out.println("La suma de la lista es "+Funciones.listaSuma(tamaño));
+
+    // System.out.println("Elige la cantidad de numero que quieres tener en la lista");
+    // tamaño = sc.nextInt();
+    // System.out.println("La media de la lista es "+Funciones.mediaLista(tamaño));
+
+    // System.out.println("Elige un numero");
+    // numero = sc.nextInt();
+    // System.out.println("La suma de los numeros pares desde "+numero+" es "+Funciones.sumaPares(numero));
+
+    // System.out.println("Elige el tamaño de la lista de numeros");
+    // tamaño = sc.nextInt();
+    // System.out.println("La suma de los numeros pares dentro de la lista son "+Funciones.listaSumaPares(tamaño));
+
+    System.out.println("Elige el tamaño de la lista de numeros");
+    Integer tamaño = sc.nextInt();
+    sc.nextLine();
+    ArrayList<Integer> lista = new ArrayList<>();
+   ;
+    System.out.println("La lista nueva de los numeros pares a partir de la lista dada es "+ Funciones.listaParesOrden(tamaño,lista,0));
+
+
     }
 }
-
+ 
 
 
--- src/mates/Funciones.java
+++ src/mates/Funciones.java
@@ -1,33 +1,128 @@
 package mates;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Funciones {
 
+
     public static int suma(int n) {
-        int acumulado = 0;
-        for (int i = 1; i <= n; i++) {
-            acumulado += i;
-        }
-        return acumulado;
-    }
-
-    public static int factorial(int n){
-        if (n<1) {
-            return 1;
-        }else{
-            return n * factorial(n-1);
-        }
-
-    }
-
-    public static int potencias(int n,int numero){
-        if (numero == 0) {
+        if (n == 0) {
             return 0;
-        }else if(n == 0){
-            return 1;
         }else{
-            return n * potencias(n, numero);
+            return n + suma(n-1);
         }
     }
 
-   
-}
+    public static int factorial(int n) {
+        if (n < 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+
+    }
+
+    public static int potencias(int n, int i) {
+
+        if (n == 0) {
+            return 1;
+        } else {
+            return i * potencias(n - 1, i);
+        }
+    }
+
+    public static Integer listaSuma(Integer tamaño){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> lista = new ArrayList<>();
+    if(tamaño > 0){
+        System.out.println("Elige el numero que quieres agregar");
+        Integer numero = sc.nextInt();
+        lista.add(numero);
+        return numero + listaSuma(tamaño - 1);
+    }else
+        return 0;
+    }
+
+    public static Integer mediaLista(Integer tamaño){
+    Scanner sc = new Scanner(System.in);
+    ArrayList<Integer> lista = new ArrayList<>();
+    
+    if(tamaño > 0){
+        System.out.println("Elige el numero que quieres agregar");
+        Integer numero = sc.nextInt();
+        lista.add(numero);
+        int suma = (numero + listaSuma(tamaño - 1));
+        return suma/tamaño;
+    }else
+        return 0;
+    }
+
+    public static int sumaPares(int n){
+        
+        int residuo = n%2;
+        if(residuo == 1){
+            n = n-1;
+        }
+        if (n<=1) {
+            return 0;
+        }else{
+            return n + sumaPares(n-2);
+            }
+        
+        
+        }
+
+        public static Integer listaSumaPares(Integer tamaño){
+            Scanner sc = new Scanner(System.in);
+            ArrayList<Integer> lista = new ArrayList<>();
+            
+        if(tamaño > 0){
+            System.out.println("Elige el numero que quieres agregar");
+            Integer numero = sc.nextInt();
+            lista.add(numero);
+            Integer residuo = numero % 2;
+            if (residuo == 0) {
+                return numero + listaSumaPares(tamaño-1);
+            }else{
+                return listaSumaPares(tamaño-1);
+            }
+            
+        }else{
+            return 0;
+        }
+
+        
+
+        }
+        public static ArrayList<Integer> listaParesOrden(int tamaño,ArrayList<Integer> listaNueva,int indice){
+
+            Scanner sc = new Scanner(System.in);
+            listaNueva = new ArrayList<>();
+            ArrayList<Integer> lista = new ArrayList<>();
+            indice = 0;
+
+            if (tamaño > 0) {
+
+            System.out.println("Elige el numero que quieres agregar");
+            Integer numero = sc.nextInt();
+            lista.add(numero);
+             
+
+            if ( lista.get(indice) % 2 == 0) {
+                listaNueva.add(numero);
+                listaParesOrden(tamaño-1,listaNueva,indice+1);
+            }else{
+                listaParesOrden(tamaño-1, listaNueva,indice+1);
+            }
+            }
+                
+            return listaNueva;
+           
+        }
+
+        public static Integer listaParesApartir(int numero){
+
+        }
+    }
+
